1c815118e9b7174a89f56626d3973e29d186b488
Igor Sousa <112203229+SalowQ@users.noreply.github.com>
2025-06-13 09:07:11 -0300

    feat: criar conteúdo de página 'em construção'

--- src/app/features/general/pages/coming-soon/coming-soon.component.ts
+++ src/app/features/general/pages/coming-soon/coming-soon.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-coming-soon',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './coming-soon.component.html',
-  styleUrl: './coming-soon.component.css'
+  styleUrl: './coming-soon.component.css',
 })
-export class ComingSoonComponent {
-
-}
+export class ComingSoonComponent {}
