a65a43cfb1cac5d110987aedf16e7a6507d4425e
Igor Sousa <112203229+SalowQ@users.noreply.github.com>
2025-06-13 17:40:31 -0300

    feat: linkar botões do menu superior

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
 import { SidebarMenuComponent } from './shared/components/sidebar-menu/sidebar-menu.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, SidebarMenuComponent],
+  imports: [RouterOutlet, SidebarMenuComponent, RouterLink],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
