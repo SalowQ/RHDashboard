951c0d9457c030feb8f6ae002ca83edbbe4bc4a0
Igor Sousa <112203229+SalowQ@users.noreply.github.com>
2025-06-16 10:05:38 -0300

    feat: atualizar formulário cadastro funcionário para usar componentes genéricos

--- src/app/features/employees/pages/register-employee/register-employee.component.ts
+++ src/app/features/employees/pages/register-employee/register-employee.component.ts
@@ -7,15 +7,30 @@ import {
 } from '@angular/forms';
 import { NgxMaskDirective } from 'ngx-mask';
 import { AlertService } from '../../../../shared/services/alert/alert.service';
+import { InputTextComponent } from '../../../../shared/components/input-text/input-text.component';
+import { InputSelectComponent } from '../../../../shared/components/input-select/input-select.component';
+import { CodeDescription } from '../../../../shared/interfaces/code-description';
 
 @Component({
   selector: 'app-register-employee',
-  imports: [ReactiveFormsModule, NgxMaskDirective],
+  imports: [ReactiveFormsModule, InputTextComponent, InputSelectComponent],
   templateUrl: './register-employee.component.html',
   styleUrl: './register-employee.component.css',
 })
 export class RegisterEmployeeComponent {
   employeeForm!: FormGroup;
+  departmentList: CodeDescription[] = [
+    { code: 1, description: 'Setor 1' },
+    { code: 2, description: 'Setor 2' },
+  ];
+  positionList: CodeDescription[] = [
+    { code: 1, description: 'Cargo 1' },
+    { code: 2, description: 'Cargo 2' },
+  ];
+  scheduleList: CodeDescription[] = [
+    { code: 1, description: 'Horário 1' },
+    { code: 2, description: 'Horário 2' },
+  ];
 
   constructor(private fb: FormBuilder, private alertService: AlertService) {}
 
